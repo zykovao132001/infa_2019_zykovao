e0c8b89048f32cc296d4910ddb11084c5ddb48e0
zykovao132001 <anton.zykov.13@mail.ru>
2019-10-23 21:30:22 +0300

    sef

--- lab4/Ball_game.py
+++ lab4/Ball_game.py
@@ -1,64 +1,99 @@
 import tkinter as tk
-from random import randrange as rnd, choice
+from random import randrange as rnd, c
 import time
-"""
--установил 3й питон 
--напиал класс меню
-
-"""
+import math
 
 root = tk.Tk()
-# geomerty(width x height)
+
 root.geometry('800x600')
 
 canv = tk.Canvas(root, bg='white')
 canv.pack(fill=tk.BOTH, expand=1)
 
-colors = ['red','orange','yellow', 'green','blue']
+colors = ['red', 'orange','yellow', 'green','blue']
+
+Balls = []
+
+g = 10
+dt = 10
+n = 10
+
+tim = 0
+
 
 class Shape:
     def __init__(self, color):
         self.color = color
-        self.x0 = rnd(100, 700)
-        self.y0 = rnd(100, 500)
+        self.x = rnd(100, 700)
+        self.y = rnd(100, 500)
         self.r = rnd(30, 50)
         self.dx = rnd(-10, 10)
         self.dy = rnd(-10, 10)
-        self.obj = None
+        self.obj = 0
 
     def draw(self):
-        self.obj = canv.create_oval(self.x0 - self.r, self.y0 - self.r,
-                                           self.x0 + self.r, self.y0 + self.r,
-                                           fill=self.color, width=0)
-        self.move()
+        for i in Balls:
+            if ((self.x - i.x) ** 2) + ((self.y - i.y) ** 2)  >= ((self.r + i.r) ** 2):
+                self.obj = canv.create_oval(self.x - self.r, self.y - self.r,
+                                            self.x + self.r, self.y + self.r,
+                                            fill=self.color, width=0)
 
     def move(self):
-        if canv.coords(self.obj)[2] >= 800 or canv.coords(self.obj)[0] <= 0:
+        for i in ball:
+            if ((self.x - i.x) ** 2) + ((self.y - i.y) ** 2) <= ((self.r + i.r) ** 2):
+                self.dx = - sefl.dx
+                self.dy = - sefl.dy
+        if (self.x - self.r) <= 0:
             self.dx = - self.dx
-        if canv.coords(self.obj)[3] >= 600 or canv.coords(self.obj)[1] <= 0:
+        if (self.x + self.r) >= 800:
+            self.dx = - self.dx
+        if (self.y - self.r) <= 0:
+            self.dy = - self.dy
+        if (self.y + self.r) >= 600:
             self.dy = - self.dy
 
-        canv.move(self.obj, self.dx, self.dy)
-        root.after(10, self.move)
-        print(self.x, self.y)
+        self.x += self.dx
+        self.y += self.dy
+
+    def axelaration(self):
+        global tim
+        global g
+        self.dx = self.dx + (g * math.cos(tim))
+        self.dy = self.dy + (g * math.sin(tim))
+        if tim >= (2 * math.pi):
+            tim = tim - 2 * math.pi
+        else:
+            tim = tim + 0.1
+
+    def show(self):
+        canvas.move(self.obj, self.dx, self.dy)
+
+def tick():
+    global dt
+
+    for i in balls:
+        ball.move()
+        ball.show()
+    root.after(dt, tick)
+
+
+class Game:
+    def __init__(self):
+        self.score = 0
+
+    def canvas_click_handler(self, event):
+        for i in balls:
+            if ((i.x - event.x) ** 2) + ((i.y - event.y) ** 2) <= i.r:
+                self.score += 1
+        print(str(self.score))
+
+    def main(self):
+        global root, canvas,
 
 
 
-ball1 = Shape(choice(colors))
-ball1.draw()
-ball2 = Shape(choice(colors))
-ball2.draw()
-ball3 = Shape(choice(colors))
-ball3.draw()
 
-def main_timer():
-    global ball1, ball2, ball3
-    root.after(10, ball1.move)
-    root.after(10, ball2.move)
-    root.after(10, ball3.move)
 
-def click(event):
-    print('click')
 
-#canv.bind('<Button-1>', click)
+
 tk.mainloop()
